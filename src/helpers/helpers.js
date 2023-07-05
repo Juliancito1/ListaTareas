8778be0492317cf86ef2a11225960f10b0f21dd2
Julian <julianser@hotmail.com.ar>
2023-07-05 14:27:09 -0300

    Se agregaron editar y obtener tarea en el archivo helpers.js

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -36,3 +36,31 @@ export const borrarTarea = async (id) => {
         
     }
 }
+
+export const editarTarea = async (tarea,id) => {
+    try {
+        const respuesta = await fetch(URL_TAREAS+'/'+id,{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(tarea)
+        });
+        return respuesta
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+export const obtenerTarea = async (id) => {
+    try{
+        const respuesta = await fetch(`${URL_TAREAS}/${id}`);
+        const tarea = await respuesta.json();
+        return tarea;
+
+    }catch(error)
+    {
+        console.log(error)
+    }
+}
