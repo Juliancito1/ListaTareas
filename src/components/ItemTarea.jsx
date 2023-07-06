7029cf5f475598133d05fccdfb80dbfb13df6866
Julian <julianser@hotmail.com.ar>
2023-07-06 18:57:57 -0300

    Se realizo la union con el BackEnd y se realizaron cambios en los componentes

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,6 +1,8 @@
-import { Button, ListGroup, Modal } from "react-bootstrap";
-import { borrarTarea, obtenerTareas } from "../helpers/helpers";
-import { useState } from "react";
+import { Button, Form, ListGroup, Modal } from "react-bootstrap";
+import { borrarTarea, editarTarea, obtenerTarea, obtenerTareas } from "../helpers/helpers";
+import { useState , useEffect} from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 const ItemTarea = ({ tarea, setTareas }) => {
   const eliminarTarea = () => {
     borrarTarea(tarea._id).then((respuesta) => {
@@ -12,10 +14,45 @@ const ItemTarea = ({ tarea, setTareas }) => {
     });
   };
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const{register,handleSubmit,formState: {errors},reset,setValue} = useForm()
+
+   const editar = (id) => {
+    useEffect(()=>{
+      obtenerTarea(id).then((respuesta) => {
+        if(respuesta)
+        {
+          setValue("tarea",respuesta.tarea)
+        }
+    })
+    })
+  }
+
+  const onSubmit = (tareaEditada) => {
+    editarTarea(tareaEditada,tarea._id).then((respuesta) => {
+      if(respuesta && respuesta.status === 200)
+      {
+        Swal.fire(
+          'Tarea actualizada',
+          'La Tarea fue actualizada',
+          'success'
+        )
+        obtenerTareas().then((respuesta) => {
+          if(respuesta)
+          {
+            setTareas(respuesta)
+          }
+        })
+      }else{
+        Swal.fire(
+          'Se produjo un error al intentar actualizar los datos',
+          'error');
+      }
+    })
+  };
+  
   return (
     <ListGroup.Item className="d-flex my-3 justify-content-between">
       <h6>{tarea.tarea}</h6>
@@ -23,7 +60,7 @@ const ItemTarea = ({ tarea, setTareas }) => {
         <Button variant="danger" onClick={eliminarTarea}>
           Borrar
         </Button>
-        <Button className="ms-md-3" variant="warning">
+        <Button className="ms-md-3" onClick={editar(tarea._id)} onClickCapture={handleShow}  variant="warning">
           Editar
         </Button>
       </div>
@@ -34,18 +71,23 @@ const ItemTarea = ({ tarea, setTareas }) => {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Editar Tarea</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don not even try to press
-          escape key.
+          <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3" controlId="tarea">
+        <Form.Control type="text" placeholder="Ingrese una tarea" {...register('tarea',{
+          required: 'Ingrese una tarea'
+        })}/>
+        <Form.Text className="text-danger">
+          {errors.tarea?.message}
+        </Form.Text>
+      </Form.Group>
+      <div className="d-flex justify-content-end">
+      <Button variant="warning" type="submit" onClick={handleClose}>Editar</Button>
+      </div>
+          </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
       </Modal>
     </ListGroup.Item>
   );
